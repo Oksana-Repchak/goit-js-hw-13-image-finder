409597f47831fde4f6692f5a8cb8518c20e2463b
Oksana Repchak <oksa.repchak@gmail.com>
2020-11-08 15:27:16 +0200

    scroll

--- src/js/imageFinder.js
+++ src/js/imageFinder.js
@@ -1,53 +1,72 @@
 import ApiService from './apiService';
 import imageTemplate from '../templates/imageCard.hbs';
+import animateScroll from 'animated-scroll-to';
 
 const refs = {
   form: document.querySelector('#search-form'),
   gallery: document.querySelector('#gallery'),
   button: document.querySelector('#load-more'),
+  imageContainer: document.querySelector('.gallery'),
 };
 
 const apiService = new ApiService();
 
 refs.form.addEventListener('submit', onSearch);
 refs.button.addEventListener('click', onLoadMore);
-refs.button.addEventListener('click', scrollTo);
 
 
 
-function onSearch(e) {
+async function onSearch(e) {
   e.preventDefault();
 
-  const inputValue = e.currentTarget.elements.query.value;
-  
-  // if (apiService.inputValue = '') {
-  //   return alert('Enter the correct value');
-  // }
+  try {
+    const inputValue = e.currentTarget.elements.query.value;
 
-  apiService.resetPage();
-  apiService.searchQuery = inputValue;
-  apiService.fetchImages().then(images => {
+    apiService.resetPage();
+    apiService.searchQuery = inputValue;
+
+   const response = await apiService.fetchImages();
     clearListImage();
-    listImageMarkup(images);
-  });
+    listImageMarkup(response);
+    // apiService.fetchImages().then(images => {
+    //   clearListImage();
+    //   listImageMarkup(images);
+    // });
+  }
+  catch (error) {
+    console.log('Error');
+  }
 }
 
 function listImageMarkup(images) {
   refs.gallery.insertAdjacentHTML('beforeend', imageTemplate(images));
 }
 
-function onLoadMore(e) {
-  apiService.fetchImages().then(listImageMarkup);
-  scrollTo();
+async function onLoadMore(e) {
+  try {
+    const response = await apiService.fetchImages();
+    listImageMarkup(response);
+    scroll();
+  }
+  catch {
+    console.log('Error');
+  }
+  // apiService.fetchImages().then(listImageMarkup);
+  // scroll();
+
 }
 
 function clearListImage() {
   refs.gallery.innerHTML = '';
 }
 
-function scrollTo() {
-    window.scrollTo({
-      top: 1000,
-      behavior: 'smooth',
-    });
+function scroll() {
+  const indexToScroll = 12 * (apiService.page - 1) - 11;
+  const itemToScroll = refs.imageContainer.children[indexToScroll];
+  const options = {
+    speed: 500,
+    verticalOffset: -10,
+  };
+
+  animateScroll(itemToScroll, options);
 }
